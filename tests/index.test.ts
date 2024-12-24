1d992d93447213c94f7cb6c82e33617d530717d9
Michael James <micrjames921@gmail.com>
2024-12-24 16:24:52 -0700

    setup project

---
+++ tests/index.test.ts
@@ -0,0 +1,11 @@
+import { Utilities } from "../Utilities";
+
+describe("A Utility Class", () => {
+   beforeAll(() => {
+   });
+   describe("With a Dot Product Inner Class", () => {
+	  test.todo("Should perform the operation.");
+	  test.todo("Should be a number.");
+	  test.todo("Should throw the error.");
+   });
+});
