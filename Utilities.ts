58a8a51ab31012bf977fc1bccad413c64f31c030
Michael James <micrjames921@gmail.com>
2024-12-25 16:16:22 -0700

    wrote tests for 'dot operation'; scaffolded class and inner class code

--- Utilities.ts
+++ Utilities.ts
@@ -1,2 +1,9 @@
 export class Utilities {
+   public dot = new class {
+	  constructor(public superThis: Utilities) {}
+
+	  public operator(a: number[], b: number[]): number {
+		 return -1;
+	  }
+   }(this);
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,29 @@
 import { Utilities } from "../Utilities";
 
 describe("A Utility Class", () => {
+   let utility: Utilities;
    beforeAll(() => {
+	  utility = new Utilities();
    });
    describe("With a Dot Product Inner Class", () => {
-	  test.todo("Should perform the operation.");
-	  test.todo("Should be a number.");
-	  test.todo("Should throw the error.");
+	  let dotOp: typeof utility.dot;
+	  beforeAll(() => {
+		 dotOp = utility.dot;
+	  });
+	  describe("A Basic Operation", () => {
+		 let result:number;
+		 beforeAll(() => {
+			result = dotOp.operator([0, 0], [0, 0]);
+		 });
+		 describe("Completed", () => {
+			test.todo("Should exist.");
+			test.todo("Should return a number.");
+			test.todo("Should perform the operation.");
+		 });
+		 describe("Throws an Error", () => {
+			test.todo("Should throw an 'Vectors are unequal length' error.");
+			test.todo("Should throw an 'Vectors are not numbers' error.");
+		 });
+	  });
    });
 });
