0f83b4e60f6b0036595b62a2dc81a19d2b66e5bd
Michael James <micrjames921@gmail.com>
2024-12-29 15:07:57 -0700

    completed tests on section 'Executed' of 'iterate' test suite

--- tests/iter.test.ts
+++ tests/iter.test.ts
@@ -9,9 +9,11 @@ describe("A Utility Class", () => {
    describe("With an Iterate Class", () => {
 	  let iterOp: typeof utility.iterate;
 	  let sb: StringBuilder;
+	  let iterOpArr: String[];
 	  beforeAll(() => {
 		 iterOp = utility.iterate;
 		 sb = new StringBuilder();
+		 iterOpArr = ["hello", "olleh"];	
 	  });
 	  describe("A Basic Operation", () => {
 		 describe("Created", () => {
@@ -20,21 +22,35 @@ describe("A Utility Class", () => {
 			});
 		 });
 		 describe("Executed", () => {
-			let iterOpArr: String[];
-			let mockCallback: (val: String, index: number) => void;
+			let mockCallback: jest.Mock<void, [String, number]>;
+			let mockCallbackCalls: [String, number][];
+			let whichCBArg: number;
 			beforeAll(() => {
-			   iterOpArr = ["hello", "olleh"];	
-			   sb.append('[');
-			   iterOp.iterate(iterOpArr, jest.fn((char, idx) => {
+			   mockCallback = jest.fn((char, idx) => {
 				  if(idx != 0) sb.appendSp();
 				  sb.append(`${char}`);
 				  if(idx != iterOpArr.length-1) sb.appendComma();
-			   }));
+			   });
+			   sb.append('[');
+			   iterOp.iterate(iterOpArr, mockCallback);
 			   sb.append(']');
+			   mockCallbackCalls = mockCallback.mock.calls;
 			});
-			test.todo("Should have executed 'two' calls.");
-			test.todo("Should have as first argument: 'hello'.");
-			test.todo("Should have as first argument: 'olleh'.");
+			test("Should have executed 'two' calls.", () => {
+			   const numCalls = mockCallbackCalls.length;
+			   const iterOpArrSize = iterOpArr.length;
+			   expect(numCalls).toBe(iterOpArrSize);
+			});
+			test("Should have as first argument: 'hello'.", () => {
+			   whichCBArg = 0;
+			   const iterOp1stArg = mockCallbackCalls[whichCBArg][0];
+			   expect(iterOp1stArg).toBe(iterOpArr[whichCBArg]);
+			});
+			test("Should have as second argument: 'olleh'.", () => {
+			   whichCBArg = 1;
+			   const iterOp2ndArg = mockCallbackCalls[whichCBArg][0];
+			   expect(iterOp2ndArg).toBe(iterOpArr[whichCBArg]);
+			});
 		 });
 		 describe("After Execution.", () => {
 			let sbStr: String;
